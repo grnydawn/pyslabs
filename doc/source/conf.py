942e3ad066b90ec346c26963204b18ba0d694cf6
Youngsung Kim <kimy@ornl.gov>
2021-12-22 16:57:21 -0500

    minor update

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,7 +24,7 @@ author = 'Youngsung Kim'
 email = 'youngsung.kim.act2@gmail.com'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.1'
+release = '0.1.3'
 
 # -- General configuration ---------------------------------------------------
 
